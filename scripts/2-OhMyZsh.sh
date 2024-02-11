cf085561fbb5f1d61725f538338eb64ef1b79048
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-12 11:50:34 +1300

    Add curl and omz install code into installation script

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
+if ! command -v "curl" &> /dev/null; then
+    sudo apt-get install curl -y
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null
 
