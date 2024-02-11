410fef264fe9e17cbad7fba4b5cbdb3d52de601a
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-12 10:58:46 +1300

    Add Zsh installation

--- install.sh
+++ install.sh
@@ -2,23 +2,42 @@
 #
 # Script to help install programs used by the dotfiles for debian based systems
 
+set -eu -o pipefail
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo privileges:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 function ask() {
     read -p "$1 (Y/n): " response
     [ -z "$response" ] || [ "$response" = "y" ]
 }
 
-for file in scripts/* 
-do
+function is_installed() {
+    command -v "$1" &> /dev/null 
+}
+
+for file in scripts/*; do
     fullpath=$(realpath $file)
 
     program=${file#"scripts/"}
+    program=${program#*-}
     program=${program%".sh"}
-    
-    if [ $program == "zsh" ] && [ $(basename $SHELL) == "zsh" ]; then
-        continue
-    fi
+   
+    if [ $program == "zsh" ]; then
+        if [ $(basename $SHELL) == "zsh" ]; then
+            continue
+        elif is_installed "zsh" && ask "Make Zsh the default shell?" ; then
+            chsh -s $(which zsh)
+            continue
+        fi
+    fi 
 
-    if ask "Install ${program}?"; then
+    if ask "Install and configure ${program}?"; then
         source ${fullpath}
     fi
+    
+    echo ""
 done
---
+++ scripts/1-zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
