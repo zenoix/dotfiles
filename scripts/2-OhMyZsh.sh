c8eb699d4fc4deed1752913f29a446bab6a197f8
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-12 13:29:28 +1300

    Add yes response to omz installation

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -4,5 +4,5 @@ if ! command -v "curl" &> /dev/null; then
     sudo apt-get install curl -y
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null
+echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
