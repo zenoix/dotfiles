50c387432a5f2b8c3cf831f81e03c1955578b91d
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 19:41:11 +1300

    Make git clone quiet

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -6,6 +6,6 @@ fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
