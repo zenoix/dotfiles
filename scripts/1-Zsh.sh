057c044d80a4651c7f293ee21a342ab70d683428
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 20:22:39 +1300

    Fix zsh installation script

--- scripts/1-Zsh.sh
+++ scripts/1-Zsh.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-if [ $(basename $SHELL) == "zsh" ]; then
-    exit
-elif is_installed "zsh"; then
-    if ask "Make Zsh the default shell?"; then
+if [ $(basename $SHELL) != "zsh" ]; then
+    if ! is_installed "zsh" && ask "Install and configure Zsh?"; then
+        sudo apt-get -qq install zsh -y
+        chsh -s $(which zsh)
+    elif ask "Zsh is already installed. Make Zsh the default shell?"; then
         chsh -s $(which zsh)
     fi
-elif ask "Install and configure ${program}?"; then
-    sudo apt-get install zsh -y
-    chsh -s $(which zsh)
 fi
-
