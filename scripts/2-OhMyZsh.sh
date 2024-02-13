001a6b3af211cea141c8ccb9272627210cc2e1ef
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 20:32:54 +1300

    Add check if omz is already installed

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
-if ! command -v "curl" &> /dev/null; then
+if ! is_installed "curl" ; then
     sudo apt-get -qq install curl -y
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [ ! -d "$HOME/.oh-my-zsh" ] || [ -z "$ZSH" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
+    echo "lol"
+    git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
