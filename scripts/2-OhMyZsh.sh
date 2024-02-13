981cac5d12ea998e0997f1ce6034fade95080482
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 20:58:23 +1300

    Add ask if user wants to install omz

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 
-if ! is_installed "curl" ; then
-    sudo apt-get -qq install curl -y
+if ask "Install and configure Oh My Zsh?" ; then
+    if ! is_installed "curl" ; then
+        sudo apt-get -qq install curl -y
+    fi
+
+    if [ ! -d "$HOME/.oh-my-zsh" ] || [ -z "$ZSH" ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    fi
+
+
+    if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
+        git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    fi
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh" ] || [ -z "$ZSH" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
-    echo "lol"
-    git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
