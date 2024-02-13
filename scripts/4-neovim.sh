6f86ca8dd7aff756960d5b05d1bc259d0fdaf850
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-14 02:02:20 +1300

    Change to only installing nvim

--- scripts/4-neovim.sh
+++ scripts/4-neovim.sh
@@ -1,13 +1,6 @@
 #!/bin/zsh
 
-if ! is_installed "nvim" ; then
-    if ask "Install and configure Neovim?" ; then
+if ! is_installed "nvim" && ask "Install Neovim?" ; then
         sudo apt-get install neovim -y
-        nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-    fi
-else
-    if ask "Neovim is already installed. Configure Neovim?" ; then
-        nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-    fi
 fi
 
