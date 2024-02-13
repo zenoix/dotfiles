2385845c86f57cf7486af8a182b9ee13eae8da4f
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 21:54:06 +1300

    Add prompts

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -1,14 +1,19 @@
 #!/bin/zsh
 
-if ask "Install and configure Oh My Zsh?" ; then
-    if ! is_installed "curl" ; then
-        sudo apt-get -qq install curl -y
-    fi
+if [ ! -d "$HOME/.oh-my-zsh" ] &&
+    [ -z "$ZSH" ] && 
+    ask "Install and configure Oh My Zsh?" ; then
 
-    if [ ! -d "$HOME/.oh-my-zsh" ] || [ -z "$ZSH" ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    fi
+if ! is_installed "curl" ; then
+    sudo apt-get -qq install curl -y
+fi
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
+    git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+else
 
     if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
         git clone -q https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
--- scripts/4-neovim.sh
+++ scripts/4-neovim.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 
-sudo apt-get -qq install neovim -y
+if ! is_installed "nvim" ; then
+    if ask "Install and configure Neovim?" ; then
+        sudo apt-get -qq install neovim -y
+        nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+    fi
+else
+    if ask "Neovim is already installed. Configure Neovim?" ; then
+        nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+    fi
+fi
 
-# Install packages automatically
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
