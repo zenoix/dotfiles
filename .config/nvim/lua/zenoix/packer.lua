a3195d4b4ca83530aea09a22b6fc1e969cee7c89
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-14 01:06:59 +1300

    Trying a different bootstrapping code

--- .config/nvim/lua/zenoix/packer.lua
+++ .config/nvim/lua/zenoix/packer.lua
@@ -1,7 +1,19 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+-- vim.cmd.packadd('packer.nvim')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -74,5 +86,9 @@ use {
 }
 
 use "lukas-reineke/indent-blankline.nvim"
-
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
