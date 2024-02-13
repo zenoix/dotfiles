b69fbb8ec2211f4b16a96cc7fe9280a7a25773c6
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 19:37:02 +1300

    Extract zsh installation

--- install.sh
+++ install.sh
@@ -14,29 +14,5 @@ zsh_enabled=false
 
 for file in $CURR_DIR/scripts/*; do
     fullpath=$(realpath $file)
-
-    program=${file#"scripts/"}
-    program=${program#*-}
-    program=${program%".sh"}
-   
-    if [ $program == "Zsh" ]; then
-        if [ $(basename $SHELL) == "zsh" ]; then
-            zsh_enabled=true
-            continue
-        elif is_installed "zsh"; then
-            if ask "Make Zsh the default shell?"; then
-                chsh -s $(which zsh)
-                zsh_enabled=true
-            fi
-            continue
-        elif ask "Install and configure ${program}?"; then
-            source ${fullpath}
-            zsh_enabled=true
-            continue
-        fi
-    fi 
-
-    if ask "Install and configure ${program}?"; then
-        source ${fullpath}
-    fi 
+    source ${fullpath}
 done
--- scripts/1-Zsh.sh
+++ scripts/1-Zsh.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo apt-get -qq install zsh -y
-chsh -s $(which zsh)
+if [ $(basename $SHELL) == "zsh" ]; then
+    exit
+elif is_installed "zsh"; then
+    if ask "Make Zsh the default shell?"; then
+        chsh -s $(which zsh)
+    fi
+elif ask "Install and configure ${program}?"; then
+    sudo apt-get install zsh -y
+    chsh -s $(which zsh)
+fi
 
-echo
