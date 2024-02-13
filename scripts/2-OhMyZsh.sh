a0aca44aba6581f9c07cbe0d6d80683d18170c27
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-13 15:45:23 +1300

    Update omz download to unattended install

--- scripts/2-OhMyZsh.sh
+++ scripts/2-OhMyZsh.sh
@@ -4,7 +4,7 @@ if ! command -v "curl" &> /dev/null; then
     sudo apt-get install curl -y
 fi
 
-echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
