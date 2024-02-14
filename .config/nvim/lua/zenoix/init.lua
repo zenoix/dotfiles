6e8c29acd927b99b8c8223ccaed09ef9f388b896
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-14 13:39:20 +1300

    Try fix the bootstrapping

--- .config/nvim/lua/zenoix/init.lua
+++ .config/nvim/lua/zenoix/init.lua
@@ -1,16 +1,3 @@
 require("zenoix.remap")
 require("zenoix.set")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("zenoix.lazy")
+require("zenoix.plugins")
