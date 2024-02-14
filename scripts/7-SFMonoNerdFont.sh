554c640c9d437221b5c246b18fdacf1b1f77a7ef
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-14 14:44:56 +1300

    Add SF Mono

---
+++ scripts/7-SFMonoNerdFont.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+if [ ! -d "$HOME/.fonts" ] ; then
+    echo "Creating $HOME/.fonts folder"
+    mkdir $HOME/.fonts
+fi
+
+echo "Installing SFMono Nerd Font"
+
+cp "$CURR_DIR/assets/SFMono Regular Nerd Font Complete.otf" "$HOME/.fonts"
+
+fc-cache -f -v
+
+echo
