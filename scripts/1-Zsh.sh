57b7fcaf4e651440bd105df7f9e48be90bef49ce
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-14 13:47:05 +1300

    Change prompts and add blank echo

--- scripts/1-Zsh.sh
+++ scripts/1-Zsh.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 if [ $(basename $SHELL) != "zsh" ]; then
-    if ! is_installed "zsh" && ask "Install and configure Zsh?"; then
+    if ! is_installed "zsh" && ask "Install Zsh and make it the dault shell?"; then
         sudo apt-get install zsh -y
+        echo "Password needed to change shell."
         chsh -s $(which zsh)
     elif ask "Zsh is already installed. Make Zsh the default shell?"; then
+        echo "Password needed to change shell."
         chsh -s $(which zsh)
     fi
+    echo
 fi
