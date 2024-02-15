a1e64b10fa45f9066c201c461a9236a46f9951ba
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-15 19:01:02 +1300

    Add node installation to finalise

--- scripts/finalise.sh
+++ scripts/finalise.sh
@@ -3,9 +3,12 @@ if is_installed "stow" && ask "Would you like to configure everything with stow?
     git reset --hard &> /dev/null
 fi
 
-
+echo "Installing tmux plugins"
 if [ -d $HOME/.tmux/plugins/tpm/ ] ; then
     $HOME/.tmux/plugins/tpm/bin/install_plugins
 fi
+
+echo "Installing node"
+nvm install node
 
 echo "Installation complete. Please restart your machine for changes to be made."
