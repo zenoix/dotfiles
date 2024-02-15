36e1759d83faef6fac88676f5642abd03d7f69d6
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-15 18:04:47 +1300

    Add linting and formatting

---
+++ .config/nvim/lua/zenoix/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				html = { "prettier" },
+				javascript = { "prettier" },
+				json = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				yaml = { "prettier" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
--- .config/nvim/lua/zenoix/plugins/lsp/mason.lua
+++ .config/nvim/lua/zenoix/plugins/lsp/mason.lua
@@ -1,33 +1,47 @@
-return {    
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function ()
-        local mason = require("mason")
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-        local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
+		local mason_tool_installer = require("mason-tool-installer")
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "bashls",
-                "lua_ls",
-                "marksman",
-                "pyright",
-                "r_language_server",
-            },
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-            automatic_installation = true,
-        })
-    end,
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"bashls",
+				"marksman",
+				"lua_ls",
+				"pyright",
+				"r_language_server",
+			},
+
+			automatic_installation = true,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"black",
+				"hadolint",
+				"isort",
+				"prettier",
+				"ruff",
+				"stylua",
+			},
+		})
+	end,
 }
