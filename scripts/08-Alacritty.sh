77a448a4f377c8443c47ba1ad6895517511f28ec
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-15 22:32:00 +1300

    Update alacritty

--- scripts/08-Alacritty.sh
+++ scripts/08-Alacritty.sh
@@ -1,7 +1,27 @@
 #!/bin/zsh
 
 if ! is_installed "alacritty" && ask "Install Alacritty?"; then
-    sudo apt-get install alacritty
-    sudo update-alternatives --config x-terminal-emulator
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+    rustup override set stable
+    rustup update stable
+
+    sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+
+    cargo build --release
+
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+
+    cd ..
+
+    rm -rf ./alacritty
+
+    sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
     echo
 fi
