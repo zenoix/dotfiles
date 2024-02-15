6a8c06be8e159d9f988e8fe4ddf10ac0f818d957
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-15 22:56:21 +1300

    Remove font install prints

--- scripts/07-SFMonoNerdFont.sh
+++ scripts/07-SFMonoNerdFont.sh
@@ -1,11 +1,8 @@
 #!/bin/zsh
 
 if [ ! -d "$HOME/.fonts" ] ; then
-    echo "Creating $HOME/.fonts folder"
     mkdir $HOME/.fonts
 fi
-
-echo "Installing SFMono Nerd Font"
 
 cp "$CURR_DIR/assets/SFMono Regular Nerd Font Complete.otf" "$HOME/.fonts"
 
