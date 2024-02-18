da169cd3db434234b4aaa398c381b8ce5cc2713c
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-02-18 22:40:23 +1300

    Fix nvim-r config

--- .config/nvim/lua/zenoix/plugins/nvim-r.lua
+++ .config/nvim/lua/zenoix/plugins/nvim-r.lua
@@ -1,14 +1,24 @@
 return {
-    "jalvesaq/Nvim-R",
-    config = function ()
+	{
+		"jalvesaq/Nvim-R",
+		branch = "master",
+		ft = { "r", "rmd", "quarto" },
+		config = function()
+			-- Start nvim-r when opening a R compatible file
+			vim.cmd("let R_auto_start = 2")
 
-        -- Start nvim-r when opening a R compatible file
-        vim.cmd('let R_auto_start = 2')
+			-- Have more syntax highlighting in the R console
+			vim.cmd("let Rout_more_colors = 1")
 
-        -- Have more syntax highlighting in the R console
-        vim.cmd('let Rout_more_colors = 1')
-
-        -- Disable _ turning into arrows 
-        vim.cmd('let R_assign = 0')
-    end
+			-- Disable _ turning into arrows
+			vim.cmd("let R_assign = 0")
+		end,
+	},
+	{
+		"jalvesaq/cmp-nvim-r",
+		ft = { "r", "rmd", "quarto" },
+		config = function()
+			require("cmp_nvim_r").setup({})
+		end,
+	},
 }
