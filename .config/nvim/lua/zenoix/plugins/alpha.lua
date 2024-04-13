f3d2600a0cc5cebd3603fd07dce1d5d6320fb3ed
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-04-13 17:36:01 +1200

    Change alpha dashboard

--- .config/nvim/lua/zenoix/plugins/alpha.lua
+++ .config/nvim/lua/zenoix/plugins/alpha.lua
@@ -1,10 +1,12 @@
 return {
 	"goolord/alpha-nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
+		Header = {
 			"                               ",
 			"                               ",
 			"       ▄▀▀▀▀▀▀▀▀▀▀▄▄           ",
@@ -32,22 +34,100 @@ return {
 			"                               ",
 		}
 
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  New File    ", ":enew<CR>"),
-			dashboard.button("f", "  Find File   ", ":Telescope find_files<CR>"),
-			dashboard.button("t", "  Find Text   ", ":Telescope live_grep<CR>"),
-			-- dashboard.button("c", "  Nvim Config ", ":Dotfiles<CR>"),
-			dashboard.button("q", "  Quit        ", ":qa<CR>"),
+		require("alpha.term")
+
+		local dynamic_header = {
+			type = "terminal",
+			command = "chafa -c full --fg-only --symbols braille ~/dotfiles/.config/nvim/static/spirited.gif",
+			width = 70,
+			height = 20,
+			opts = {
+				position = "center",
+				redraw = true,
+				window_config = {},
+			},
 		}
 
-		dashboard.section.footer.val = {
+		local text_header = {
+			type = "text",
+			val = Header,
+			opts = {
+				position = "center",
+				hl = "Type",
+			},
+		}
+
+		-- local default_header = nil
+		-- local ret = "command -v chafa"
+
+		-- if ret == 0 then
+		-- 	default_header = dynamic_header
+		-- else
+		-- 	default_header = text_header
+		-- end
+
+		local buttons = {
+			type = "group",
+			val = {
+				dashboard.button("n", "  New File    ", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("f", "  Find File   ", ":Telescope find_files<CR>"),
+				dashboard.button("t", "  Find Text   ", ":Telescope live_grep<CR>"),
+				dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
+				dashboard.button(
+					"c",
+					"  Nvim Config ",
+					":cd $HOME/dotfiles/.config/nvim | Telescope find_files<CR>"
+				),
+				dashboard.button("q", "  Quit        ", ":qa<CR>"),
+			},
+			opts = {
+				spacing = 1,
+			},
+		}
+
+		Footer = {
 			"                ",
 			"It is what it is",
 			"                ",
 		}
 
+		local footer = {
+			type = "text",
+			val = Footer,
+			opts = {
+				position = "center",
+				hl = "Number",
+			},
+		}
+
+		local section = {
+			header = dynamic_header,
+			buttons = buttons,
+			footer = footer,
+		}
+
+		local opts = {
+			layout = {
+				{ type = "padding", val = 4 },
+				section.header,
+				{ type = "padding", val = 1 },
+				section.buttons,
+				{ type = "padding", val = 1 },
+				section.footer,
+				{ type = "padding", val = 4 },
+			},
+			opts = {
+				margin = 3,
+				noautocmd = true,
+				redraw_on_resize = true,
+			},
+		}
+
+		dashboard.opts = opts
+		-- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 		alpha.setup(dashboard.opts)
 
-		require("alpha").setup(require("alpha.themes.dashboard").config)
+		-- Disable folding on alpha buffer
+		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
 	end,
 }
