d46759d4cc30d12d140f9847ab56d13258b29c0e
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-04-13 17:55:11 +1200

    Add ripgrep

--- .config/nvim/lua/zenoix/plugins/telescope.lua
+++ .config/nvim/lua/zenoix/plugins/telescope.lua
@@ -8,7 +8,6 @@ return {
 			prompt_prefix = " ",
 			path_display = { "smart" },
 			dynamic_preview_title = true,
-			sorting_strategy = "ascending",
 			layout_config = {
 				prompt_position = "bottom",
 				height = 0.95,
@@ -19,6 +18,27 @@ return {
 					["l"] = "select_default",
 				},
 			},
+
+			-- configure to use ripgrep
+			vimgrep_arguments = {
+				"rg",
+				"--follow", -- Follow symbolic links
+				"--hidden", -- Search for hidden files
+				"--no-heading", -- Don't group matches by each file
+				"--with-filename", -- Print the file path with the matched lines
+				"--line-number", -- Show line numbers
+				"--column", -- Show column numbers
+				"--smart-case", -- Smart case search
+
+				-- Exclude some patterns from search
+				"--glob=!**/.git/*",
+				"--glob=!**/.idea/*",
+				"--glob=!**/.vscode/*",
+				"--glob=!**/build/*",
+				"--glob=!**/dist/*",
+				"--glob=!**/yarn.lock",
+				"--glob=!**/package-lock.json",
+			},
 		},
 		file_ignore_patterns = {
 			"lazy-lock.json",
@@ -26,6 +46,20 @@ return {
 		pickers = {
 			find_files = {
 				hidden = true,
+				-- needed to exclude some files & dirs from general search
+				-- when not included or specified in .gitignore
+				find_command = {
+					"rg",
+					"--files",
+					"--hidden",
+					"--glob=!**/.git/*",
+					"--glob=!**/.idea/*",
+					"--glob=!**/.vscode/*",
+					"--glob=!**/build/*",
+					"--glob=!**/dist/*",
+					"--glob=!**/yarn.lock",
+					"--glob=!**/package-lock.json",
+				},
 			},
 			oldfiles = {
 				initial_mode = "normal",
