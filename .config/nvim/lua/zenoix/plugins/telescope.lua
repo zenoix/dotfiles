d6023c4e87a72136e87d0ec135a0b8b829338fbb
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-04-13 17:36:09 +1200

    Change telescope settings

--- .config/nvim/lua/zenoix/plugins/telescope.lua
+++ .config/nvim/lua/zenoix/plugins/telescope.lua
@@ -1,9 +1,39 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.4",
+	tag = "0.1.5",
 	-- or                            , branch = '0.1.x',
 	dependencies = { { "nvim-lua/plenary.nvim" } },
-	config = function()
+	opts = {
+		defaults = {
+			prompt_prefix = " ",
+			path_display = { "smart" },
+			dynamic_preview_title = true,
+			sorting_strategy = "ascending",
+			layout_config = {
+				prompt_position = "bottom",
+				height = 0.95,
+			},
+			mappings = {
+				n = {
+					["q"] = "close",
+					["l"] = "select_default",
+				},
+			},
+		},
+		file_ignore_patterns = {
+			"lazy-lock.json",
+		},
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+			oldfiles = {
+				initial_mode = "normal",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("telescope").setup(opts)
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
