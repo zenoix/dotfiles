7edbad472dbde05820003a97fcd3b64eb6cae067
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-04-26 13:33:08 +1200

    Add notification to unpair toggle

--- .config/nvim/lua/zenoix/plugins/mini/mini_pairs.lua
+++ .config/nvim/lua/zenoix/plugins/mini/mini_pairs.lua
@@ -10,7 +10,13 @@ return {
 		{
 			"<leader>up",
 			function()
+				vim.notify = require("notify")
 				vim.g.minipairs_disable = not vim.g.minipairs_disable
+				if vim.g.minipairs_disable then
+					vim.notify("Disabled auto pairs", "warn", { title = "Option" })
+				else
+					vim.notify("Enabled auto pairs", "info", { title = "Option" })
+				end
 			end,
 			desc = "Toggle Auto Pairs",
 		},
