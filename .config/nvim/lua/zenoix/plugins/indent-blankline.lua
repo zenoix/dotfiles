30cc98e4c971e86091925af5601355ecafd4a335
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-04-26 13:14:40 +1200

    Add indent-blankline options

--- .config/nvim/lua/zenoix/plugins/indent-blankline.lua
+++ .config/nvim/lua/zenoix/plugins/indent-blankline.lua
@@ -1,7 +1,28 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	opts = {},
+	opts = {
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		scope = { enabled = false },
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+		},
+	},
 	config = function()
 		require("ibl").setup()
 	end,
