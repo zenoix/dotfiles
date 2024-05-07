ff1bc1c10f0d67acfce7fc0fddbecd57769ee8ec
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-05-07 22:04:27 +1200

    Add oil.nvim

--- .config/nvim/lua/zenoix/core/remap.lua
+++ .config/nvim/lua/zenoix/core/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Allows moving of lines using J or K when selected
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -13,11 +14,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- When pasting over something, keep the original thing in the clipboard
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Change terminal emulator normal mode key to esc
 vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n>")
