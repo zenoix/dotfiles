ad4d9047cde92bb73bdba57f03a34ccb5cb2af2a
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-05-16 10:51:32 +1200

    Fix git commands

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env zsh
 
-latest_remote=$(git ls-remote origin -h refs/heads/main |cut -f1)
-echo "Lol"
+latest_remote=$(git -C $HOME/dotfiles ls-remote origin -h refs/heads/main |cut -f1)
 
-if [[ "`git log --pretty=%H ...refs/heads/main^ | head -n 1`" != $latest_remote ]] && [[ $DOT_LATEST_IGNORED_COMMIT != $latest_remote ]] ; then
+if [[ "`git -C $HOME/dotfiles log --pretty=%H ...refs/heads/main^ | head -n 1`" != $latest_remote ]] && [[ $DOT_LATEST_IGNORED_COMMIT != $latest_remote ]] ; then
     print -P "%B%F{green}Info:%f%b Dotfiles update available.\n"
 
     if read -q 'choice?Would you like to update now? (y/n) '; then
