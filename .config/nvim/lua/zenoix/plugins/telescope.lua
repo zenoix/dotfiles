77a3e4526d62789249335a116ba9c2799e7e7f9c
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-06-13 00:21:16 +1200

    Update telescope config

--- .config/nvim/lua/zenoix/plugins/telescope.lua
+++ .config/nvim/lua/zenoix/plugins/telescope.lua
@@ -1,57 +1,32 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	-- or                            , branch = '0.1.x',
-	dependencies = { { "nvim-lua/plenary.nvim" } },
-	opts = {
-		defaults = {
-			prompt_prefix = " ",
-			path_display = { "smart" },
-			dynamic_preview_title = true,
-			-- layout_config = {
-			-- 	prompt_position = "bottom",
-			-- 	height = 0.95,
-			-- },
-			mappings = {
-				n = {
-					["q"] = "close",
-					["l"] = "select_default",
+	tag = "0.1.8",
+	-- or                              , branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local opts = {
+			defaults = {
+				path_display = { "smart" },
+				dynamic_preview_title = true,
+				mappings = {
+					n = {
+						["q"] = "close",
+						["l"] = "select_default",
+					},
 				},
-			},
 
-			-- configure to use ripgrep
-			vimgrep_arguments = {
-				"rg",
-				"--follow", -- Follow symbolic links
-				"--hidden", -- Search for hidden files
-				"--no-heading", -- Don't group matches by each file
-				"--with-filename", -- Print the file path with the matched lines
-				"--line-number", -- Show line numbers
-				"--column", -- Show column numbers
-				"--smart-case", -- Smart case search
-
-				-- Exclude some patterns from search
-				"--glob=!**/.git/*",
-				"--glob=!**/.idea/*",
-				"--glob=!**/.vscode/*",
-				"--glob=!**/build/*",
-				"--glob=!**/dist/*",
-				"--glob=!**/yarn.lock",
-				"--glob=!**/package-lock.json",
-			},
-		},
-		file_ignore_patterns = {
-			"lazy-lock.json",
-		},
-		pickers = {
-			find_files = {
-				hidden = true,
-				-- needed to exclude some files & dirs from general search
-				-- when not included or specified in .gitignore
-				find_command = {
+				-- configure to use ripgrep
+				vimgrep_arguments = {
 					"rg",
-					"--files",
-					"--hidden",
+					"--follow", -- Follow symbolic links
+					"--hidden", -- Search for hidden files
+					"--no-heading", -- Don't group matches by each file
+					"--with-filename", -- Print the file path with the matched lines
+					"--line-number", -- Show line numbers
+					"--column", -- Show column numbers
+					"--smart-case", -- Smart case search
+
+					-- Exclude some patterns from search
 					"--glob=!**/.git/*",
 					"--glob=!**/.idea/*",
 					"--glob=!**/.vscode/*",
@@ -61,12 +36,32 @@ return {
 					"--glob=!**/package-lock.json",
 				},
 			},
-			oldfiles = {
-				initial_mode = "normal",
+			file_ignore_patterns = {
+				"lazy-lock.json",
 			},
-		},
-	},
-	config = function(_, opts)
+			pickers = {
+				find_files = {
+					hidden = true,
+					-- needed to exclude some files & dirs from general search
+					-- when not included or specified in .gitignore
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/yarn.lock",
+						"--glob=!**/package-lock.json",
+					},
+				},
+				oldfiles = {
+					initial_mode = "normal",
+				},
+			},
+		}
 		require("telescope").setup(opts)
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
