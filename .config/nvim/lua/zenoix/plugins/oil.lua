73e380f09d73a2348e751a9fb8884ca40c1ffc05
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-06-22 01:49:57 +1200

    Change close to Esc

--- .config/nvim/lua/zenoix/plugins/oil.lua
+++ .config/nvim/lua/zenoix/plugins/oil.lua
@@ -7,6 +7,9 @@ return {
 		require("oil").setup({
 			default_file_explorer = true,
 			skip_confirm_for_simple_edits = true,
+			keymaps = {
+				["<Esc>"] = "actions.close",
+			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
