010e59a2b7e27a35b020a6e42d55f538bc5f142f
Zenoix <41812358+Zenoix@users.noreply.github.com>
2024-06-23 01:48:14 +1200

    Add fzf customization

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -12,8 +12,22 @@ source $HOME/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 eval "$(zoxide init zsh)"
 
 
+# fzf keybindings and fuzzy finding
+eval "$(fzf --zsh)"
+
+# fzf git
+source $HOME/.config/zsh/plugins/fzf-git.sh/fzf-git.sh
 
 
+# Use fd for fzf
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
+_fzf_compgen_path() {
+    fd --hidden --exclude .git . "$1"
+}
 
-
+_fzf_compgen_dir() {
+    fd --type=d --hidden --exclude .git . "$1"
+}
