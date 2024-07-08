0ff362b266f63fbd46b88aaf8eb11a7e9c4a0b62
zenoix <41812358+zenoix@users.noreply.github.com>
2024-07-09 00:08:38 +1200

    Add telescope diagnostics keymap

--- .config/nvim/lua/zenoix/plugins/telescope.lua
+++ .config/nvim/lua/zenoix/plugins/telescope.lua
@@ -67,6 +67,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>pd", builtin.diagnostics, {})
 		vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>pm", ":Telescope harpoon marks<CR>", {})
