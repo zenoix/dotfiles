5f734649ab07302beb9a177a919bcefa1b641d0f
zenoix <41812358+zenoix@users.noreply.github.com>
2024-07-12 15:01:58 +1200

    Add fzf options

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -19,6 +19,26 @@ eval "$(fzf --zsh)"
 # fzf git
 source $HOME/.config/zsh/plugins/fzf-git.sh/fzf-git.sh
 
+export FZF_DEFAULT_OPTS="
+--border sharp
+--prompt '∷ '
+--info=inline
+--height=70%
+--multi
+--bind 'ctrl-a:select-all'
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+"
+
+export FZF_CTRL_T_OPTS="
+--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+--bind 'ctrl-p:toggle-preview'
+"
+
+export FZF_CTRL_R_OPTS="
+--layout=reverse
+"
 
 # Use fd for fzf
 export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
