390a13edfdf91ae3cae70098a19d82764fb62de4
zenoix <41812358+zenoix@users.noreply.github.com>
2024-07-24 17:41:28 +1200

    Allow for author highlighting in todo comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -6,5 +6,8 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		highlight = {
+			pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
+		},
 	},
 }
